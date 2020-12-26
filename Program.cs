47fa278631a02c17542b6fb13902fe5170cbaed0
James Ross <git@james-ross.co.uk>
2020-12-26 00:32:04 +0000

    docs: Add synopsis and example output to readme

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace OpenTTD_IRC_Info
         const int DaysPerWeek = 7;
         const int MSPerWeek = MSPerTick * TicksPerDay * DaysPerWeek;
 
-        static async void Main(string ottdServer, string ircServer, string channel, int ottdPort = 3979, int ircPort = 6667, string ircNickname = "OTTDBot")
+        static async void Main(string ottdServer, string ircServer, string ircChannel, int ottdPort = 3979, int ircPort = 6667, string ircNickname = "OTTDBot")
         {
             var ottd = new UdpClient(ottdServer, ottdPort);
             var irc = new IRC.Client(ircServer, ircPort, ircNickname);
@@ -34,13 +34,13 @@ namespace OpenTTD_IRC_Info
                     if (year % 10 == 0)
                     {
                         var companies = String.Join(", ", serverDetailInfo.Companies.Select(c => $"{c.Name} ({c.Money:N0} {(c.Income >= 0 ? '+' : '-')}= {Math.Abs(c.Income):N0})"));
-                        await irc.WriteCommand($"PRIVMSG {channel} :{year} - {companies}");
+                        await irc.WriteCommand($"PRIVMSG {ircChannel} :{year} - {companies}");
                     }
 
                     var companiesInTrouble = serverDetailInfo.Companies.Where(c => c.Income < 0 && c.Money < -2 * c.Income);
                     foreach (var c in companiesInTrouble)
                     {
-                        await irc.WriteCommand($"PRIVMSG {channel} :{year} - {c.Name} might be in trouble! Money: {c.Money:N0} Yearly income: {c.Income:N0}");
+                        await irc.WriteCommand($"PRIVMSG {ircChannel} :{year} - {c.Name} might be in trouble! Money: {c.Money:N0} Yearly income: {c.Income:N0}");
                     }
                 }
                 lastYear = year;
--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # OpenTTD IRC Info
 
 Command-line application to periodically print out company information to IRC.
+
+## Synopsis
+
+```
+dotnet run [options]
+```
+
+## Options
+
+- `--ottd-server <ottd-server>`
+
+  OpenTTD server hostname or IP address.
+
+- `--ottd-port <ottd-port>`
+
+  OpenTTD server port (default: 3979).
+
+- `--irc-server <irc-server>`
+
+  IRC server hostname or IP address.
+
+- `--irc-port <irc-port>`
+
+  IRC server port (default: 6667). TLS is not supported currently.
+
+- `--irc-nickname <irc-nickname>`
+
+  IRC nickname (default: OTTDBot).
+
+- `--irc-channel <irc-channel>`
+
+  IRC channel name (with prefix, e.g. `"#openttd"`).
+
+## Example IRC output
+
+```
+<OTTDBot> 1990 - Silver Transport (128,135 += 20,482), Other Company Name (123,456 += 12,345)
+<OTTDBot> 1991 - Silver Transport might be in trouble! Money: 113,085 Yearly income: -118,366
+```
